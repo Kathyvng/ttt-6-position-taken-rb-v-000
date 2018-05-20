ea3558c7802c0244505d149a637d019e1b7b820e
Kathy Nguyen <kathyvng@gmail.com>
2018-05-20 16:13:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,9 +5,7 @@ def position_taken?(board, index)
 end
 
 def position_taken?(board, index)
- if  board[index] == X || board[index] == O
-  true
- elsif board[index] == " " || board[index] == ""
+ if  board[index] == " " || board[index] == ""
   false
  elsif board[index] == nil
   false
